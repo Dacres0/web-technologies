21456d45c60b97d2551418386ea43e88930ba311
Dacres0 <da24aby@herts.ac.uk>
2025-11-03 13:14:54 +0000

    Refactor Webex room monitoring logic

--- space_iss.py
+++ space_iss.py
@@ -3,7 +3,9 @@ import json
 import time
 from iso3166 import countries
 
+
 choice = input("Do you wish to use the hard-coded Webex token? (y/n) ")
+
 if choice.lower() == 'n':
     user_token = input("Please enter your Webex access token: ")
     accessToken = f"Bearer {user_token}"
@@ -13,7 +15,7 @@ else:
 r = requests.get("https://webexapis.com/v1/rooms", headers={"Authorization": accessToken})
 
 if r.status_code != 200:
-    raise Exception("Incorrect reply from Webex API. Status code: {}. Text: {}".format(r.status_code, r.text))
+    raise Exception(f"Incorrect reply from Webex API. Status code: {r.status_code}. Text: {r.text}")
 
 print("\nList of available rooms:")
 rooms = r.json()["items"]
@@ -23,21 +25,23 @@ for room in rooms:
 while True:
     roomNameToSearch = input("Which room should be monitored for the /seconds messages? ")
     roomIdToGetMessages = None
+    roomTitleToGetMessages = None
 
     for room in rooms:
         if room["title"].find(roomNameToSearch) != -1:
-            print("Found room with the word " + roomNameToSearch)
+            print(f"Found room with the word {roomNameToSearch}")
             print(room["title"])
             roomIdToGetMessages = room["id"]
             roomTitleToGetMessages = room["title"]
-            print("Found room: " + roomTitleToGetMessages)
+            print(f"Found room: {roomTitleToGetMessages}")
             break
 
     if roomIdToGetMessages is None:
-        print("Sorry, I didn't find any room with " + roomNameToSearch + " in it.")
+        print(f"Sorry, I didn't find any room with {roomNameToSearch} in it.")
         print("Please try again...")
     else:
         break
+
 
 while True:
     time.sleep(1)
@@ -48,9 +52,10 @@ while True:
                      headers={"Authorization": accessToken})
 
     if r.status_code != 200:
-        raise Exception("Incorrect reply from Webex API. Status code: {}. Text: {}".format(r.status_code, r.text))
+        raise Exception(f"Incorrect reply from Webex API. Status code: {r.status_code}. Text: {r.text}")
 
     json_data = r.json()
+
     if len(json_data["items"]) == 0:
         print("No messages found in this room yet. Waiting for a new message...")
         continue
@@ -59,8 +64,8 @@ while True:
     message = messages[0]["text"]
     print(f"Latest message received: {message}")
 
-    if message.find("/") == 0:
-        if (message[1:].isdigit()):
+    if message.startswith("/"):
+        if message[1:].isdigit():
             seconds = int(message[1:])
         else:
             print("Invalid command format. Please enter /<number> (e.g. /5).")
@@ -74,7 +79,7 @@ while True:
         r = requests.get("http://api.open-notify.org/iss-now.json")
         json_data = r.json()
 
-        if r.status_code != 200 or json_data["message"] != "success":
+        if r.status_code != 200 or json_data.get("message") != "success":
             print("Error retrieving ISS location data.")
             continue
 
@@ -83,6 +88,7 @@ while True:
         timestamp = json_data["timestamp"]
         timeString = time.ctime(timestamp)
 
+       
         mapsAPIGetParameters = {
             "key": "MGVmNDVhNzktODJkYi00OTZkLWJiYTEtOGJlYTk1YTI5NjZkOTYxMGYyM2UtMDU2_P0A1_636b97a0-b0af-4297-b0e7-480dd517b3f9",
             "lat": lat,
@@ -103,9 +109,13 @@ while True:
         CityResult = address.get("city", address.get("town", "Unknown"))
         StreetResult = address.get("road", "Unknown")
 
-        if not CountryResult == "XZ":
-            CountryResult = countries.get(CountryResult).name
+        if CountryResult != "XZ":
+            try:
+                CountryResult = countries.get(CountryResult).name
+            except KeyError:
+                pass
 
+        
         if CountryResult == "XZ":
             responseMessage = (
                 f"On {timeString}, the ISS was flying over a body of water "
@@ -124,6 +134,7 @@ while True:
 
         print("Sending to Webex: " + responseMessage)
 
+       
         HTTPHeaders = {
             "Authorization": accessToken,
             "Content-Type": "application/json"
@@ -139,11 +150,11 @@ while True:
                           headers=HTTPHeaders)
 
         if r.status_code != 200:
-            print("Failed to post message to Webex. Status code: {}, Text: {}".format(r.status_code, r.text))
+            print(f"Failed to post message to Webex. Status code: {r.status_code}, Text: {r.text}")
         else:
             print("Message successfully posted to Webex.")
 
-                
+
 
 
 
